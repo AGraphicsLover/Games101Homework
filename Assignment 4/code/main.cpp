dfd7eb71e901dfaa690c21074fb66b6b458aed8c
AGraphicsLover <neonrainkeqing@gmail.com>
2022-08-08 02:12:08 -0700

    完成作业4，并实现了反走样，完成了实验报告

--- Assignment 4/code/main.cpp
+++ Assignment 4/code/main.cpp
@@ -33,8 +33,15 @@ void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window)
 cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, float t) 
 {
     // TODO: Implement de Casteljau's algorithm
-    return cv::Point2f();
-
+    if(control_points.size()==1)
+        return cv::Point2f(control_points[0]);
+    std::vector<cv::Point2f> points;
+    for (int i =1;i<control_points.size();i++)
+    {
+        points.emplace_back(control_points[i].x*t+control_points[i-1].x*(1-t),control_points[i].y*t+control_points[i-1].y*(1-t));
+    }
+    return recursive_bezier(points,t);
+    
 }
 
 void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
@@ -42,6 +49,40 @@ void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window)
     // TODO: Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's 
     // recursive Bezier algorithm.
 
+    for (double t = 0; t < 1; t+=0.001)
+    {
+        auto point = recursive_bezier(control_points, t);
+        window.at<cv::Vec3b>(point.y, point.x)[1] = 255;
+    }
+    
+}
+
+void bezier_interpolated(const std::vector<cv::Point2f> &control_points, cv::Mat &window)
+{
+    for (double t = 0; t < 1; t += 0.001)
+    {
+        auto point = recursive_bezier(control_points, t);
+
+        cv::Point2i p0(point.x-std::floor(point.x)<0.5?std::floor(point.x):std::ceil(point.x),
+                        point.y - std::floor(point.y)<0.5?std::floor(point.y):std::ceil(point.y));
+        std::vector<cv::Point2i> p_around = {p0,cv::Point2i(p0.x-1,p0.y),
+                            cv::Point2i(p0.x,p0.y-1),cv::Point2i(p0.x-1,p0.y-1)};
+        float sum_d = 0.f;
+        float max_d = std::sqrt(2);
+        std::vector<float> dis;
+        for(int i = 0;i<4;i++)
+        {
+            cv::Point2f pp(p_around[i].x+0.5f,p_around[i].y+0.5f);
+            float d = max_d - std::sqrt(std::pow(point.x-pp.x,2)+std::pow(point.y-pp.y,2));
+            dis.push_back(d);
+            sum_d += d;
+        }
+        for(int i = 0;i<4;i++)
+        {
+            float w = dis[i]/sum_d;
+            window.at<cv::Vec3b>(p_around[i].y, p_around[i].x)[1] = std::min(255.f,window.at<cv::Vec3b>(p_around[i].y, p_around[i].x)[1]+255.f*w);
+        }
+    }
 }
 
 int main() 
@@ -62,11 +103,11 @@ int main()
 
         if (control_points.size() == 4) 
         {
-            naive_bezier(control_points, window);
-            //   bezier(control_points, window);
+            // naive_bezier(control_points, window);
+            bezier_interpolated(control_points, window);
 
             cv::imshow("Bezier Curve", window);
-            cv::imwrite("my_bezier_curve.png", window);
+            cv::imwrite("Have interpolation.png", window);
             key = cv::waitKey(0);
 
             return 0;
