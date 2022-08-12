6d1aeae7889b87e2eca3c8a23a80a4f8517298e0
AGraphicsLover <neonrainkeqing@gmail.com>
2022-08-11 20:51:43 -0700

    已完成作业8，并上传了作业8报告

--- Assignment 8/src/rope.cpp
+++ Assignment 8/src/rope.cpp
@@ -17,6 +17,17 @@ namespace CGL {
 //        for (auto &i : pinned_nodes) {
 //            masses[i]->pinned = true;
 //        }
+        Vector2D step = (end - start)/(num_nodes-1);
+        for(int i = 0;i<num_nodes;i++)
+        {
+            Mass *mass = new Mass(start+i*step,node_mass,false);
+            mass->velocity = Vector2D(0.f,0.f);
+            masses.push_back(mass);
+            if(i!=0)
+                springs.push_back(new Spring(masses[i-1],masses[i],k));
+        }
+        for (auto &i : pinned_nodes) 
+            masses[i]->pinned = true;
     }
 
     void Rope::simulateEuler(float delta_t, Vector2D gravity)
@@ -24,6 +35,11 @@ namespace CGL {
         for (auto &s : springs)
         {
             // TODO (Part 2): Use Hooke's law to calculate the force on a node
+
+            Vector2D dis = s->m2->position-s->m1->position;
+            Vector2D f = -s->k*dis.unit()*(dis.norm()-s->rest_length);
+            s->m1->forces -= f;
+            s->m2->forces += f;
         }
 
         for (auto &m : masses)
@@ -33,6 +49,15 @@ namespace CGL {
                 // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
 
                 // TODO (Part 2): Add global damping
+
+                m->forces += gravity*m->mass;
+                float k_d = 0.01f;
+                Vector2D f_d = -k_d * m->velocity;
+                m->forces += f_d;
+                Vector2D a = m->forces/m->mass;
+                m->velocity += a * delta_t;
+                m->position += m->velocity * delta_t;
+                
             }
 
             // Reset all forces on each mass
@@ -45,17 +70,28 @@ namespace CGL {
         for (auto &s : springs)
         {
             // TODO (Part 3): Simulate one timestep of the rope using explicit Verlet （solving constraints)
+            Vector2D dis = s->m2->position-s->m1->position;
+            Vector2D f = -s->k*dis.unit()*(dis.norm()-s->rest_length);
+            s->m1->forces -= f;
+            s->m2->forces += f;
         }
 
         for (auto &m : masses)
         {
             if (!m->pinned)
             {
+                m->forces += gravity*m->mass;
+                Vector2D a = m->forces/m->mass;
                 Vector2D temp_position = m->position;
                 // TODO (Part 3.1): Set the new position of the rope mass
-                
+                float damping_factor = 0.00005f;
                 // TODO (Part 4): Add global Verlet damping
+
+                m->position += (1-damping_factor)*
+                    (m->position-m->last_position)+a*delta_t*delta_t;
+                m->last_position = temp_position;
             }
+            m->forces = Vector2D(0, 0);
         }
     }
 }
